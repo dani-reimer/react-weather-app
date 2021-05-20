a7d6ec3a53c01dae2f3de822840bd9ed8afc01f1
dani-reimer <78756494+dani-reimer@users.noreply.github.com>
2021-05-20 15:42:39 -0400

    Imported Google Font Montserrat

--- src/Header.js
+++ src/Header.js
@@ -3,25 +3,13 @@ import React from "react";
 export default function Header() {
   return (
     <div>
-      {" "}
-      <head>
-        {" "}
+      
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
           crossorigin="anonymous"
         ></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-          rel="stylesheet"
-        ></link>
-        <script
-          src="https://kit.fontawesome.com/01a1136ea3.js"
-          crossorigin="anonymous"
-        ></script>
-      </head>{" "}
     </div>
   );
 }
