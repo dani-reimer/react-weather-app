3b5f772bfa92371232a1c1ba09ac560bb0757fde
dani-reimer <78756494+dani-reimer@users.noreply.github.com>
2021-05-27 12:11:09 -0400

    Updated .css files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Body from "./Body";
 import Footer from "./Footer";
 
+
 export default function App() {
   return (
     <div className="App">
--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Forecast.css";
 import ForecastDate from "./ForecastDate";
 
 export default function Forecast() {
