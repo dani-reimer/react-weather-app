8027599c1acc35286588d1726c7a0112d4d61166
dani-reimer <78756494+dani-reimer@users.noreply.github.com>
2021-05-27 11:43:48 -0400

    Updated title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import "./App.css";
-import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
-      <title>Weather</title>
-      <Header />
+      <title>Weather Forecast</title>
       <Body />
       <Footer />
     </div>
